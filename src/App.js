69b626f258d4c0ea0b2c7710722e0f94f75affbc
Anupam kumar <anupamkumar740@gmail.com>
2023-12-01 10:22:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { LineChart, XAxis, Tooltip, CartesianGrid, Line } from "recharts";
+
+const CandlestickChart = ({ data, label }) => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    setChartData((prevChartData) => {
+      const newData = [
+        ...prevChartData,
+        {
+          time: new Date().toLocaleTimeString(),
+          value: data[label], 
+        },
+      ];
+
+      
+      if (newData.length > 10) {
+        newData.shift();
+      }
+
+      return newData;
+    });
+  }, [data, label]);
+
+  return (
+    <LineChart
+      width={600}
+      height={400}
+      data={chartData}
+      margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+    >
+      <XAxis dataKey="time" />
+      <Tooltip />
+      <CartesianGrid stroke="#f5f5f5" />
+      <Line type="monotone" dataKey="value" stroke="#ff7300" />
+    </LineChart>
+  );
+};
 
 function App() {
+  const [trading, setTrading] = useState({});
+
+  useEffect(() => {
+    const socketURL =
+      "wss://functionup.fintarget.in/ws?id=fintarget-functionup";
+    const socket = new WebSocket(socketURL);
+
+    socket.addEventListener("open", (event) => {
+      console.log("WebSocket connected:", event);
+    });
+
+    socket.addEventListener("message", (event) => {
+      try {
+        const newData = JSON.parse(event.data);
+        console.log(event.data)
+        setTrading(newData);
+      } catch (error) {
+        console.error('Error parsing WebSocket message:', error);
+      }
+    });
+
+    socket.addEventListener("close", (event) => {
+      console.log("WebSocket close:", event);
+    });
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{JSON.stringify(trading)}</div>
+      {Object.keys(trading).map((key) => (
+        <CandlestickChart key={key} data={trading} label={key} />
+      ))}
     </div>
   );
 }
 
-export default App;
+export default App;
